1064070c73238fff53978bb417691a3a1a859554
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-01 10:58:07 +0530

    userdata saved at database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,7 @@ import User from "../models/userModel.js";
 
 
 
+
 const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
@@ -16,10 +17,20 @@ res.status(401)
 throw new Error('User Already Exist')
 }
 const user=await User.create({name,email,password})
+if(user){
+  res.status(201).json({
+    _id:user._id,
+    name:user.name,
+    email:user.email,
+    password:user.password
+    
+  })}
+else{
+  res.status(400)
+  throw new Error('invalid user')
+  
+}
 
- 
-  console.log(name,email,password,userExist);
-  res.status(200).json({ message: "Register User" });
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcryptjs'
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -18,6 +18,16 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+
+userSchema.pre('save',async function(next){
+if(!this.isModified('password')){
+    next()
+}
+
+const salt=await bcrypt.genSalt(10)
+this.password=await bcrypt.hash(this.password,salt)
+} )
 
 const User=mongoose.model('User',userSchema)
 export default User
