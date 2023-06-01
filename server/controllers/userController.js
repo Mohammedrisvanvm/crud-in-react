148845eec51e5c0d3b9350953d6262adc329c4f8
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-01 15:33:40 +0530

    created authuser and compared bcrypted  password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,37 +2,44 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
-
-
-
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const {email,password}=req.body
+  const user=await User.findOne({email})
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
+  } else {
+    res.status(401);
+    throw new Error("invalid email or password");
+  }
 });
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-  const {name,email,password}=req.body
-
-const userExist=await User.findOne({email})
-if(userExist){
-res.status(401)
-throw new Error('User Already Exist')
-}
-const user=await User.create({name,email,password})
-if(user){
-  generateToken(res,user._id)
-  res.status(201).json({
-    _id:user._id,
-    name:user.name,
-    email:user.email,
-    password:user.password
-    
-  })}
-else{
-  res.status(400)
-  throw new Error('invalid user')
-  
-}
-
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(401);
+    throw new Error("User Already Exist");
+  }
+  const user = await User.create({ name, email, password });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid user");
+  }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,15 +1,16 @@
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
-  const token = Jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
+  console.log(token);
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "developement",
-    sameSite:'strict',
-    maxAge:30*24*60*60*1000
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
 
-export default generateToken
+export default generateToken;
