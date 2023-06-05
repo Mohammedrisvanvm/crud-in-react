371f1a73141082ee9cacb624bbda13d41d665e60
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-05 17:21:05 +0530

    created authslice reducer

--- frontend/src/slices/AuthSlice.jsx
+++ frontend/src/slices/AuthSlice.jsx
@@ -5,3 +5,19 @@ const initialState = {
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
 };
+const Authslice=createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        setCredentials:(state,action)=>{
+            state.userInfo=action.payload;
+            localStorage.setItem('userinfo',JSON.stringify(action.payload))
+        },
+        logout:(state,action)=>{
+            state.userInfo=null
+            localStorage.removeItem('userinfo')
+        }
+    }
+})
+
+export default Authslice.reducer
