0f9edc2b0bf3cb62e98ff72c92240f71d9cfc3bb
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-05 11:15:26 +0530

    login and register created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/header'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <div>
       <Header/>
+      <HomeScreen/>
     </div>
   )
 }
