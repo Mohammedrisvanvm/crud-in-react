f05ab04ac11ead28a9c22b47dbbe26c774dbcb9d
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-05 18:45:50 +0530

    imported useNavigate and useDispatch

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Form, Button, Row, Col,Container,Card } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 const LoginForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
   
+    const navigate=useNavigate()
+    const dispatch=useDispatch()
     const submitHandler = async (e) => {
       e.preventDefault();
       console.log(email);
