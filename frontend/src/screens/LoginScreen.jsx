a9a69e0dcf1a831cd6f37490604544e9cef9ca03
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-05 11:44:39 +0530

    Login Route setted

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header'
+import LoginForm from '../components/LoginForm'
+
+const LoginScreen = () => {
+  return (
+  <>
+  <Header/>
+  <LoginForm/>
+  </>
+  )
+}
+
+export default LoginScreen
