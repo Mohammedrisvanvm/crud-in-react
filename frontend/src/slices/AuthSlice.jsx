d4abf2799d83d59cd859656558f81ef755cb49cc
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-06 16:31:50 +0530

    adminauth slice created

--- frontend/src/slices/AuthSlice.jsx
+++ frontend/src/slices/AuthSlice.jsx
@@ -5,6 +5,11 @@ const initialState = {
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
 };
+const initialState2 = {
+  userInfo: localStorage.getItem("adimininfo")
+    ? JSON.parse(localStorage.getItem("admininfo"))
+    : null,
+};
 const Authslice=createSlice({
     name:'auth',
     initialState,
@@ -19,6 +24,22 @@ const Authslice=createSlice({
         }
     }
 })
+const AdminAuthslice=createSlice({
+    name:'adminAuth',
+    initialState2,
+    reducers:{
+        AdminsetCredentials:(state,action)=>{
+            state.adminInfo=action.payload;
+            localStorage.setItem('admininfo',JSON.stringify(action.payload))
+        },
+        Adminlogout:(state,action)=>{
+            state.adminInfo=null
+            localStorage.removeItem('admininfo')
+        }
+    }
+})
+
+export const {AdminsetCredentials,Adminlogout}=AdminAuthslice.actions
 export const {setCredentials,logout}= Authslice.actions
 
 export default Authslice.reducer
--- frontend/src/slices/adminApiSlice.jsx
+++ frontend/src/slices/adminApiSlice.jsx
@@ -1,2 +1,24 @@
 import { apiSlice } from "./apiSlice";
-const USER_URL='/admin'
+const ADMIN_URL='/admin'
+
+export const adminApiSlice=apiSlice.injectEndpoints({
+    endpoints:(builder)=>({
+        login:builder.mutation({
+            query:(data)=>({
+                url:`${ADMIN_URL}/admin`,
+                method: 'POST',
+                body:data,
+            })
+        }),
+        logout:builder.mutation({
+            query:(data)=>({
+                url:`${ADMIN_URL}/admin/logout`,
+                method: 'POST',
+                body:data,
+            })
+        }),
+
+
+
+    })
+})
