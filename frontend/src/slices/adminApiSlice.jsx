cc6dd033c77b3b259192a915c435c9164e87cf0d
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-06 16:17:10 +0530

    adminslice created

---
+++ frontend/src/slices/adminApiSlice.jsx
@@ -0,0 +1,2 @@
+import { apiSlice } from "./apiSlice";
+const USER_URL='/admin'
--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,8 +1,8 @@
 import User from "../models/userModel.js"
 
 export const adminHome=async(req,res)=>{
-    console.log('hai');
+    
     const userInfo=await User.find()
-    console.log(userInfo);
+  
     res.json(userInfo)
 }
