1d0bdd9bb22fc4dee658f42162dc899c45214cd1
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-06 16:32:56 +0530

    adminauth slice added data

--- frontend/src/slices/AuthSlice.jsx
+++ frontend/src/slices/AuthSlice.jsx
@@ -5,11 +5,7 @@ const initialState = {
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
 };
-const initialState2 = {
-  userInfo: localStorage.getItem("adimininfo")
-    ? JSON.parse(localStorage.getItem("admininfo"))
-    : null,
-};
+
 const Authslice=createSlice({
     name:'auth',
     initialState,
@@ -24,22 +20,9 @@ const Authslice=createSlice({
         }
     }
 })
-const AdminAuthslice=createSlice({
-    name:'adminAuth',
-    initialState2,
-    reducers:{
-        AdminsetCredentials:(state,action)=>{
-            state.adminInfo=action.payload;
-            localStorage.setItem('admininfo',JSON.stringify(action.payload))
-        },
-        Adminlogout:(state,action)=>{
-            state.adminInfo=null
-            localStorage.removeItem('admininfo')
-        }
-    }
-})
 
-export const {AdminsetCredentials,Adminlogout}=AdminAuthslice.actions
+
+
 export const {setCredentials,logout}= Authslice.actions
 
 export default Authslice.reducer
