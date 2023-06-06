513430e64cba12a41d111dd8c5fc056f5a4a9b8f
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-06 16:02:34 +0530

    admin ui completed

--- frontend/src/components/AdminHeader.jsx
+++ frontend/src/components/AdminHeader.jsx
@@ -6,17 +6,10 @@ function AdminHeader() {
     <header>
     <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
       <Container>
-        <Navbar.Brand href='/'>MERN App</Navbar.Brand>
+        <Navbar.Brand href='/'>CRUD APP</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ms-auto'>
-            <Nav.Link href='/login'>
-              <FaSignInAlt /> Sign In
-            </Nav.Link>
-            <Nav.Link href='/login'>
-              <FaSignOutAlt /> Sign Up
-            </Nav.Link>
-          </Nav>
+         
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- frontend/src/components/UserInfoTable.jsx
+++ frontend/src/components/UserInfoTable.jsx
@@ -1,33 +1,83 @@
-import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Container, Card } from "react-bootstrap";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import LogoutIcon from "@mui/icons-material/Logout";
+import axios from "axios";
 
 function UserInfoTable() {
-  useEffect(async ()=>{
-   let user= axios.get('/')
-  })
+  const [userInfo, setUserInfo] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/admin").then((response) => {
+      console.log(response.data);
+      setUserInfo(response.data);
+    });
+  }, [setUserInfo]);
 
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Age</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => ( 
-            <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.age}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <div
+        style={{ display: "flex", justifyContent: "flex-end", margin: "50px" }}
+      >
+        <Button variant="contained" startIcon={<AddIcon />} size="small">
+          Create User
+        </Button>
+      </div>
+      <Container className="d-flex justify-content-center">
+        <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Edit</TableCell>
+                  <TableCell>Delete</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {userInfo.map((row, index) => (
+                  <TableRow key={row.id}>
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>{row.name}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                    <TableCell>
+                      <IconButton>
+                        <EditIcon />
+                      </IconButton>
+                    </TableCell>
+                    <TableCell>
+                      <IconButton>
+                        <DeleteIcon color="error" />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Card>
+      </Container>
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "50px" }}
+      >
+        <Button variant="contained" startIcon={<LogoutIcon />} size="small">
+          Logout
+        </Button>
+      </div>
+    </>
   );
 }
 
